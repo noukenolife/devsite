b798b87b5bc3d7edd83fffb3f084e12e048c36e5
j-yamamoto <j-yamamoto@onu.edu>
2019-03-24 17:43:19 +0900

    ran lint:fix

--- packages/core/__tests__/adapters/article/InMemoryArticleRepository.ts
+++ packages/core/__tests__/adapters/article/InMemoryArticleRepository.ts
@@ -1,7 +1,7 @@
-import InMemoryRepository from "../../support/InMemoryRepository";
-import ArticleId from "@/domain/article/ArticleId";
-import Article from "@/domain/article/Article";
-import * as uuid from "uuid";
+import InMemoryRepository from '../../support/InMemoryRepository';
+import ArticleId from '@/domain/article/ArticleId';
+import Article from '@/domain/article/Article';
+import * as uuid from 'uuid';
 
 export default class InMemoryArticleRepository extends InMemoryRepository<ArticleId, Article> {
   public async nextId(): Promise<ArticleId> {
--- packages/core/__tests__/application/article/DeleteArticle.test.ts
+++ packages/core/__tests__/application/article/DeleteArticle.test.ts
@@ -1,17 +1,17 @@
-import DeleteArticle from "@/application/article/DeleteArticle";
-import Article from "@/domain/article/Article";
-import ArticleId from "@/domain/article/ArticleId";
-import InMemoryArticleRepository from "../../adapters/article/InMemoryArticleRepository";
+import DeleteArticle from '@/application/article/DeleteArticle';
+import Article from '@/domain/article/Article';
+import ArticleId from '@/domain/article/ArticleId';
+import InMemoryArticleRepository from '../../adapters/article/InMemoryArticleRepository';
 
-describe("DeleteArticle", () => {
-  it("deletes an article", async () => {
-    const article1 = new Article(new ArticleId("1"), "Title1", "Content1");
-    const article2 = new Article(new ArticleId("2"), "Title2", "Content2");
+describe('DeleteArticle', () => {
+  it('deletes an article', async () => {
+    const article1 = new Article(new ArticleId('1'), 'Title1', 'Content1');
+    const article2 = new Article(new ArticleId('2'), 'Title2', 'Content2');
 
-    const articleRepository = new InMemoryArticleRepository([ article1, article2 ]);
+    const articleRepository = new InMemoryArticleRepository([article1, article2]);
     const service = new DeleteArticle(articleRepository);
 
-    await service.invoke({ id: "1" });
-    expect(articleRepository.entities).toEqual([ article2 ]);
+    await service.invoke({ id: '1' });
+    expect(articleRepository.entities).toEqual([article2]);
   });
 });
